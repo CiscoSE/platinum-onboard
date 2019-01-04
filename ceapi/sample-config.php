1f9f31e8396f6437ce774d8886c45b3d7b5993d7
technologyordie <adam@technologyordie.com>
2019-01-03 23:12:14 -0500

    CEAPI module addition

---
+++ ceapi/sample-config.php
@@ -0,0 +1,11 @@
+<?php
+
+//telepresense endpoint service account username and password
+//must be admin
+$username = 'username';  
+$password = "password";
+
+//The broker / core service IP address and port number if not 80
+$broker = "192.168.1.11:8080";
+
+?>
