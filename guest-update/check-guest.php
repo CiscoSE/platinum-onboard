e53334386b0ebf2878266967111fe92ad446aec6
Jason Beltrame <jbeltrame@gmail.com>
2019-01-11 16:28:45 -0500

    Finished commenting.

--- guest-update/check-guest.php
+++ guest-update/check-guest.php
@@ -1,9 +1,7 @@
 <?php
-
 //
 // Global Variables
 //
-
 date_default_timezone_set('EST');
 $startdate=date('m/d/Y 00:01');
 $enddate=date('m/d/Y 23:59');
@@ -11,7 +9,6 @@ $post_header = array(
   "Content-Type: application/vnd.com.cisco.ise.identity.guestuser.2.0+xml",
   "Accept: application/vnd.com.cisco.ise.identity.guestuser.2.0+xml"
 );
-
 //
 // Start of functions Section
 //
@@ -34,7 +31,6 @@ function random_str(
     }
     return $str;
 }
-
 //
 // API Function for send back to broker
 //
@@ -83,7 +79,7 @@ function checkISE($url,$headers)
 }
 
 //
-// API Function for ISE cURL for POST
+// API Function for ISE cURL and POST
 //
 function postISE($url,$post_string,$post_header)
 {
@@ -102,6 +98,9 @@ function postISE($url,$post_string,$post_header)
   return $error_msg;
 }
 
+//
+// API Function for ISE cURL and PUT
+//
 function putISE($url,$post_string,$post_header)
 {
   $ch = curl_init();
@@ -126,7 +125,7 @@ function putISE($url,$post_string,$post_header)
 //
 
 //
-// Check ISE to see if User exists
+// Check ISE to see if User exists based on passed email ID
 //
 if(isset($_SERVER['REQUEST_METHOD'] ))
 {
@@ -136,21 +135,23 @@ if(isset($_SERVER['REQUEST_METHOD'] ))
   $headers = [
     'Accept: application/vnd.com.cisco.ise.identity.guestuser.2.0+xml'
   ];
-
+  //
   // Check ISE to see if User already array_key_exists
-
+  //
   $retOutput=checkISE($url,$headers);
-
+  //
   // Take data and parse it to see if it exists
+  //
   $p = xml_parser_create();
   xml_parse_into_struct($p, $retOutput, $vals, $index);
   xml_parser_free($p);
-
   $passed_id=$vals[2]['attributes']['ID'];
   $passed_email=$vals[2]['attributes']['NAME'];
-
   if ($passed_email != $emailaddy)
   {
+    //
+    // Email address doesn't exist, so we need to create
+    //
     echo "\r\nCreating User....\r\n\r\n";
 	  $passwd=random_str(10);
 	  $post_string = '<?xml version="1.0" encoding="utf-8" standalone="yes"?>
@@ -173,7 +174,6 @@ if(isset($_SERVER['REQUEST_METHOD'] ))
 	    <portalId>c945bfc2-f761-11e8-a29a-aa0cee21782f</portalId>
 	    <sponsorUserName>python-guest</sponsorUserName>
 	   </ns0:guestuser>';
-
     $url= "https://python-guest:LkjLkj@192.168.1.129:9060/ers/config/guestuser";
     $retError=postISE($url,$post_string,$post_header);
     if (!empty($retError))
@@ -183,8 +183,10 @@ if(isset($_SERVER['REQUEST_METHOD'] ))
 	  }
 	  else
 	  {
+      //
       // Need to  call dbAPI and update status to created
-		  $dbURL="http://24.239.120.11:9999/api/update-status-guest-account?emailid=".$emailaddy."&status=Completed&guestpassword=".$passwd;
+      //
+      $dbURL="http://24.239.120.11:9999/api/update-status-guest-account?emailid=".$emailaddy."&status=Completed&guestpassword=".$passwd;
       $message=updateAPI($dbURL);
       echo $message;
     }
@@ -209,6 +211,9 @@ if(isset($_SERVER['REQUEST_METHOD'] ))
       $status=$vals[20]['value'];
 	    if($status == "EXPIRED")
 	    {
+        //
+        //  User exists but is EXPIRED.  So we need to re-enable
+        //
         $url = "https://python-guest:LkjLkj@192.168.1.129:9060/ers/config/guestuser/" . $passed_id;
 	      $passwd=random_str(10);
 	      $post_string = '<?xml version="1.0" encoding="utf-8" standalone="yes"?>
@@ -231,6 +236,9 @@ if(isset($_SERVER['REQUEST_METHOD'] ))
 		          <portalId>c945bfc2-f761-11e8-a29a-aa0cee21782f</portalId>
 		          <sponsorUserName>python-guest</sponsorUserName>
 		        </ns0:guestuser>';
+        //
+        //  Need to update ISE guest user DB now.
+        //
         $retError=putISE($url,$post_string,$post_header);
 	      if (!empty($retError))
         {
@@ -238,6 +246,9 @@ if(isset($_SERVER['REQUEST_METHOD'] ))
         }
 	      else
 	      {
+          //
+          // Need to  call dbAPI and update status to created
+          //
 	        echo "User Re-enabled.\r\n\r\n";
 	        $dbURL="http://24.239.120.11:9999/api/update-status-guest-account?emailid=".$emailaddy."&status=Updated&guestpassword=".$passwd;
           $message=updateAPI($dbURL);
@@ -247,18 +258,32 @@ if(isset($_SERVER['REQUEST_METHOD'] ))
     }
     elseif($status == "AWAITING_INITIAL_LOGIN")
     {
+      //
+      //  If here, the user already exists and is active.
+      //
 	    echo "\r\nAccount is not expired and already active\r\n\r\n";
     }
     else
     {
+      //
+      //  If here, there was an unknown status from ISE.
+      //
 	    echo "\r\nError in status.  Unknown status found\r\n\r\n";
     }
+  }
+  else
+  {
+    //
+    //  If here, there was an error with the username compare from the script to ISE.
+    //
+    echo "\r\nError in username compare.";
   }
 }
 else
 {
- echo "ERROR in the progam!  Bailing";
+  //
+  //  If here, there was no email ID that was passed from the broker.
+  //
+  echo "No email ID passed to the API.";
 }
-
-
 ?>
