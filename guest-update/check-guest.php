5a99b2b09f746bf2ff95a0d1e5d35dda2a0b591f
Jason Beltrame <jbeltrame@gmail.com>
2019-01-11 16:15:49 -0500

    Bug fixes in POST and PUT

--- guest-update/check-guest.php
+++ guest-update/check-guest.php
@@ -1,8 +1,9 @@
 <?php
 
 //
-// Globals
+// Global Variables
 //
+
 date_default_timezone_set('EST');
 $startdate=date('m/d/Y 00:01');
 $enddate=date('m/d/Y 23:59');
@@ -12,10 +13,12 @@ $post_header = array(
 );
 
 //
-// Start of functions DOMCdataSection
+// Start of functions Section
 //
 
-// Random Password Generator
+//
+// Random Password Generator for ISE Guest User
+//
 function random_str(
     $length,
     $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -32,7 +35,9 @@ function random_str(
     return $str;
 }
 
-// API Updater
+//
+// API Function for send back to broker
+//
 function updateAPI($APIurl)
 {
   $retmsg='';
@@ -54,10 +59,75 @@ function updateAPI($APIurl)
 }
 
 //
+// API function for ISE cURL for GET
+//
+function checkISE($url,$headers)
+{
+  $ch = curl_init();
+
+  // Set URL and other appropriate options
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+  // Connect to URL and pull down information from API
+  $output = curl_exec($ch);
+  if ( ! $output )
+  {
+    print curl_errno($ch) .':'. curl_error($ch);
+  }
+  curl_close($ch);
+  return $output;
+}
+
+//
+// API Function for ISE cURL for POST
+//
+function postISE($url,$post_string,$post_header)
+{
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $post_string);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_FAILONERROR, true);
+  $output = curl_exec($ch);
+  $error_msg = curl_error($ch);
+  curl_close($ch);
+  return $error_msg;
+}
+
+function putISE($url,$post_string,$post_header)
+{
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $post_string);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $output = curl_exec($ch);
+  $error_msg = curl_error($ch);
+  curl_close($ch);
+  return $error_msg;
+}
+//
 // End of functions section
 //
 
+//
+// Start of main
+//
 
+//
+// Check ISE to see if User exists
+//
 if(isset($_SERVER['REQUEST_METHOD'] ))
 {
   parse_str($_SERVER['QUERY_STRING'], $output);
@@ -67,25 +137,13 @@ if(isset($_SERVER['REQUEST_METHOD'] ))
     'Accept: application/vnd.com.cisco.ise.identity.guestuser.2.0+xml'
   ];
 
-  $ch = curl_init();
+  // Check ISE to see if User already array_key_exists
 
-  // set URL and other appropriate options
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $retOutput=checkISE($url,$headers);
 
-  // grab URL and pass it to the browser
-  $output = curl_exec($ch);
-  if ( ! $output )
-  {
-    print curl_errno($ch) .':'. curl_error($ch);
-  }
-  curl_close($ch);
-
+  // Take data and parse it to see if it exists
   $p = xml_parser_create();
-  xml_parse_into_struct($p, $output, $vals, $index);
+  xml_parse_into_struct($p, $retOutput, $vals, $index);
   xml_parser_free($p);
 
   $passed_id=$vals[2]['attributes']['ID'];
@@ -116,20 +174,9 @@ if(isset($_SERVER['REQUEST_METHOD'] ))
 	    <sponsorUserName>python-guest</sponsorUserName>
 	   </ns0:guestuser>';
 
-    $ch = curl_init();
-	  $url2= "https://python-guest:LkjLkj@192.168.1.129:9060/ers/config/guestuser";
-	  curl_setopt($ch, CURLOPT_URL, $url2);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_string);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_FAILONERROR, true);
-    $output = curl_exec($ch);
-    $error_msg = curl_error($ch);
-    curl_close($ch);
-    if (!empty($error_msg))
+    $url= "https://python-guest:LkjLkj@192.168.1.129:9060/ers/config/guestuser";
+    $retError=postISE($url,$post_string,$post_header);
+    if (!empty($retError))
 	  {
       echo "Error in POST....(".$error_msg.") exiting";
 		  exit;
@@ -144,39 +191,28 @@ if(isset($_SERVER['REQUEST_METHOD'] ))
   }
   elseif ($passed_email == $emailaddy)
   {
+    //
+    // This ELSEIF means the user already exists as a guest user.  In this case, we need to re-enable the user.
+    //
+    echo "\r\nRe-enabling the User....\r\n\r\n";
     $status="0";
     $url = "https://python-guest:LkjLkj@192.168.1.129:9060/ers/config/guestuser/" . $passed_id;
     $headers = [
       'Accept: application/vnd.com.cisco.ise.identity.guestuser.2.0+xml'
 	  ];
-	  $ch = curl_init();
-	  curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_FAILONERROR, true);
-    $output = curl_exec($ch);
-    $error_msg = curl_error($ch);
-    curl_close($ch);
-    if (!empty($error_msg))
+    $retOutput=checkISE($url,$headers);
+	  $p = xml_parser_create();
+    xml_parse_into_struct($p, $retOutput, $vals, $index);
+    xml_parser_free($p);
+    if(array_key_exists("value",$vals[20]))
     {
-      echo "Error in POST....(".$error_msg.") exiting";
-    }
-    else
-    {
-      $p = xml_parser_create();
-      xml_parse_into_struct($p, $output, $vals, $index);
-      xml_parser_free($p);
-      if(array_key_exists("value",$vals[20]))
-      {
-        $status=$vals[20]['value'];
-	if($status == "EXPIRED")
-	{
-          $url = "https://python-guest:LkjLkj@192.168.1.129:9060/ers/config/guestuser/" . $passed_id;
-	  $passwd=random_str(10);
-	  $post_string = '<?xml version="1.0" encoding="utf-8" standalone="yes"?>
-	    <ns0:guestuser xmlns:ns0="identity.ers.ise.cisco.com">
+      $status=$vals[20]['value'];
+	    if($status == "EXPIRED")
+	    {
+        $url = "https://python-guest:LkjLkj@192.168.1.129:9060/ers/config/guestuser/" . $passed_id;
+	      $passwd=random_str(10);
+	      $post_string = '<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+	      <ns0:guestuser xmlns:ns0="identity.ers.ise.cisco.com">
 		          <customFields>
 		          </customFields>
 		          <guestAccessInfo>
@@ -195,50 +231,34 @@ if(isset($_SERVER['REQUEST_METHOD'] ))
 		          <portalId>c945bfc2-f761-11e8-a29a-aa0cee21782f</portalId>
 		          <sponsorUserName>python-guest</sponsorUserName>
 		        </ns0:guestuser>';
-
-	  $ch = curl_init();
-          curl_setopt($ch, CURLOPT_URL, $url);
-	  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-	  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-  	  curl_setopt($ch, CURLOPT_POSTFIELDS, $post_string);
-	  curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-	  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
- 	  $output = curl_exec($ch);
-          $error_msg = curl_error($ch);
-          curl_close($ch);
-          if (!empty($error_msg))
-          {
-            echo "Error in POST....(".$error_msg.") exiting";
-          }
-	  else
-	  {
-	    echo "User Enabled.\r\n\r\n";
-	    $dbURL="http://24.239.120.11:9999/api/update-status-guest-account?emailid=".$emailaddy."&status=Updated&guestpassword=".$passwd;
-            $message=updateAPI($dbURL);
-            echo $message;
-	  }
-	}
-      }
-      elseif($status == "AWAITING_INITIAL_LOGIN")
-      {
-	echo "\r\nAccount is not expired and already active\r\n\r\n";
-      }
-      else
-      {
-	echo "\r\nError in status.  Unknown status found\r\n\r\n";
-      }
+        $retError=putISE($url,$post_string,$post_header);
+	      if (!empty($retError))
+        {
+          echo "Error in POST....(".$error_msg.") exiting";
+        }
+	      else
+	      {
+	        echo "User Re-enabled.\r\n\r\n";
+	        $dbURL="http://24.239.120.11:9999/api/update-status-guest-account?emailid=".$emailaddy."&status=Updated&guestpassword=".$passwd;
+          $message=updateAPI($dbURL);
+          echo $message;
+	      }
+	    }
     }
-  }
-  else
-  {
-	   echo "ERROR in the progam!  Bailing";
+    elseif($status == "AWAITING_INITIAL_LOGIN")
+    {
+	    echo "\r\nAccount is not expired and already active\r\n\r\n";
+    }
+    else
+    {
+	    echo "\r\nError in status.  Unknown status found\r\n\r\n";
+    }
   }
 }
 else
 {
-  echo "Error in POST!";
+ echo "ERROR in the progam!  Bailing";
 }
 
-?>
 
+?>
